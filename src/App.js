556dc145e8c634c0a56b6671d6d5c58a3249938e
Konakanchi Lakshmi Supraja <59195095+konakanchi-supraja@users.noreply.github.com>
2021-03-19 16:11:43 +0530

    saga

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      news:[]
+    }
+    this.fetchData = this.fetchData.bind(this);
+  }
+  fetchData() {
+
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.fetchData}
+        >
+          fetch news
+        </button>
+        <div>
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
